796132f733427d5812abe1263ae5c6fc6ec2bb17
santosh <santosh.ece06@gmail.com>
2021-07-03 09:56:50 +0530

    redux basics.

--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,12 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Page2() {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state);
+
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
 
   return (
     <div>
       <h1>Page2</h1>
       <div className="alert alert-secondary">{state.counter}</div>
+
+      <input
+        type="button"
+        value="Increment"
+        onClick={increment}
+        className="btn btn-secondary w-100 mb-1"
+      />
+
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae
         odio blanditiis ratione quaerat deleniti possimus iusto quo? Quos iure
--- src/pages/Page3.js
+++ src/pages/Page3.js
@@ -1,13 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Page3() {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state);
+
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
 
   return (
     <div>
       <h1>Page3</h1>
 
       <div className="alert alert-secondary">{state.counter}</div>
+
+      <input
+        type="button"
+        value="Increment"
+        onClick={increment}
+        className="btn btn-secondary w-100 mb-1"
+      />
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
         consequatur sit libero sequi aut repellendus delectus earum tempore,
