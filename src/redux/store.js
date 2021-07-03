e650e5554bcc878ed22c58b65aa673acfa9964d7
santosh <santosh.ece06@gmail.com>
2021-07-03 11:24:57 +0530

    redux thunk setup.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "@reduxjs/toolkit";
+import { createStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 
 const initState = {
   counter: 100,
@@ -11,14 +12,17 @@ const DECREMENT_ACTION_TYPE = "DECREMENT";
 const ADD_TODO_ACTION_TYPE = "ADD_TODO";
 
 export function incrementAction() {
+  // WE ARE ONLY UPDTING THE UI
   return { type: INCREMENT_ACTION_TYPE };
 }
 
 export function decrementAction() {
+  // WE ARE ONLY UPDTING THE UI
   return { type: DECREMENT_ACTION_TYPE };
 }
 
 export function addTodoAction(payload) {
+  // WE ARE ONLY UPDTING THE UI
   return { type: ADD_TODO_ACTION_TYPE, payload: payload };
 }
 
@@ -41,5 +45,6 @@ function AppReducer(state = initState, action) {
   }
 }
 
-const store = createStore(AppReducer);
+// const store = createStore(AppReducer);
+const store = createStore(AppReducer, applyMiddleware(thunk));
 export { store };
